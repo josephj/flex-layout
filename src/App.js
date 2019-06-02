42b341a0ad2a5e62b2e10d574a7a8aa357f2bddb
josephj <josephj6802@gmail.com>
2019-06-03 08:21:03 +1000

    Init commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,97 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import { Flex, Box, Text } from "rebass";
+import MediaQuery from "react-responsive";
+import { ThemeProvider } from "styled-components";
+import theme from "./theme";
+import { watchViewport, unwatchViewport, getViewportState } from "tornis";
 
 function App() {
+  const [viewportSize, setViewportSize] = useState({ x: 0, y: 0 });
+  const handleViewportUpdate = state => {
+    console.log("handleViewportUpdate", state);
+    setViewportSize(state.size);
+  };
+
+  useEffect(() => {
+    // const state = getViewportState();
+    // console.log("useEffect", state.size);
+    // setViewportSize(state.size);
+
+    watchViewport(handleViewportUpdate);
+    return () => {
+      unwatchViewport(handleViewportUpdate);
+    };
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <ThemeProvider theme={theme}>
+      <>
+        <Flex flexDirection="column" style={{ height: "100%" }}>
+          <Flex bg="blue" alignItems="center" p={3}>
+            Header
+          </Flex>
+          <Flex flex={2} flexDirection={["column-reverse", "row"]}>
+            <Box
+              bg="orange"
+              p={2}
+              width={[1, 350]}
+              style={{ whiteSpace: "nowrap", overflow: "auto" }}
+            >
+              <img
+                src="https://c1.staticflickr.com/9/8387/8638813125_3cac0dc01c_n.jpg"
+                width="174"
+                alt="parrot"
+                style={{ marginRight: "1px" }}
+              />
+              <img
+                src="https://c1.staticflickr.com/9/8387/8638813125_3cac0dc01c_n.jpg"
+                width="174"
+                alt="parrot"
+                style={{ marginRight: "1px" }}
+              />
+              <img
+                src="https://c1.staticflickr.com/9/8387/8638813125_3cac0dc01c_n.jpg"
+                width="174"
+                alt="parrot"
+                style={{ marginRight: "1px" }}
+              />
+            </Box>
+            <Box bg="gray" flex={2} p={2}>
+              Design Canvas
+            </Box>
+            <MediaQuery query={`(min-width: ${theme.breakpoints[0]})`}>
+              <Box bg="purple" p={2} width={40}>
+                Toolbar
+              </Box>
+            </MediaQuery>
+          </Flex>
+        </Flex>
+        <Box
+          style={{
+            position: "absolute",
+            right: "10px",
+            top: "10px",
+            whiteSpace: "nowrap",
+            width: "60px",
+            fontSize: "12px"
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <Text
+            bg="magenta"
+            style={{ borderRadius: "4px", display: "inline-block" }}
+          >
+            {viewportSize.x}
+          </Text>
+          x
+          <Text
+            bg="magenta"
+            style={{ borderRadius: "4px", display: "inline-block" }}
+          >
+            {viewportSize.y}
+          </Text>
+        </Box>
+      </>
+    </ThemeProvider>
   );
 }
 
---
+++ src/theme.js
@@ -0,0 +1,17 @@
+export default {
+  space: [0, 4, 8, 12, 16, 24, 32],
+  breakpoints: ["640px", "1024px", "1200px", "1440px"],
+  fontSizes: [10, 12, 13, 16, 20, 28, 48, 64, 80],
+  colors: {
+    blue: "#07c",
+    lightgray: "#f6f6ff"
+  },
+  fonts: {
+    sans: "system-ui, sans-serif",
+    mono: "Menlo, monospace"
+  },
+  shadows: {
+    small: "0 0 4px rgba(0, 0, 0, .125)",
+    large: "0 0 24px rgba(0, 0, 0, .125)"
+  }
+};
