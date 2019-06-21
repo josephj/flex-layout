8d6a3c7df9290bf66bfa0e19528275f304942faa
Joseph Chiang <joseph.chiang@whitech.com.au>
2019-06-21 15:58:45 +1000

    Add more draggable example

--- src/App.js
+++ src/App.js
@@ -5,6 +5,15 @@ import styled, { ThemeProvider } from "styled-components";
 import theme from "./theme";
 import { watchViewport, unwatchViewport } from "tornis";
 import CurrentWidth from './CurrentWidth';
+import {polyfill} from "mobile-drag-drop";
+import {scrollBehaviourDragImageTranslateOverride} from "mobile-drag-drop/scroll-behaviour";
+import  { ReactComponent as Logo } from './logo.svg';
+
+// options are optional ;)
+polyfill({
+    // use this to make use of the scroll behaviour
+    dragImageTranslateOverride: scrollBehaviourDragImageTranslateOverride
+});
 
 const DesignCanvas = () => (
   <Box bg="gray" flex={2} p={2}>
@@ -46,25 +55,36 @@ function App() {
                 p={2}
                 width={[1, 200]}
                 style={{ whiteSpace: "nowrap", overflow: "auto" }}
+                alingItems="center"
+                flexWrap="nowrap"
               >
+                <Card draggable bg="yellow" mr={[1, 0]} mb={[0, 1]} width={['174px', 1]} style={{height: '115px', display: 'inline-block'}}>
+                </Card>
+                <Card draggable bg="red" mr={[1, 0]} mb={[0, 1]} width={['174px', 1]} style={{height: '115px', display: 'inline-block'}}>
+                  <Logo/>
+                </Card>
                 <PhotoCard>
                   <img
                     src="https://c1.staticflickr.com/9/8387/8638813125_3cac0dc01c_n.jpg"
                     width="174"
+                    height="115"
                     alt="parrot"
+                  />
+                </PhotoCard>
+                <PhotoCard draggable>
+                  <img
+                    src="https://c1.staticflickr.com/9/8387/8638813125_3cac0dc01c_n.jpg"
+                    width="174"
+                    height="115"
+                    alt="parrot"
+                    style={{border: '2px solid green', boxSizing: 'border-box'}}
                   />
                 </PhotoCard>
                 <PhotoCard>
                   <img
                     src="https://c1.staticflickr.com/9/8387/8638813125_3cac0dc01c_n.jpg"
                     width="174"
-                    alt="parrot"
-                  />
-                </PhotoCard>
-                <PhotoCard>
-                  <img
-                    src="https://c1.staticflickr.com/9/8387/8638813125_3cac0dc01c_n.jpg"
-                    width="174"
+                    height="115"
                     alt="parrot"
                   />
                 </PhotoCard>
@@ -72,6 +92,7 @@ function App() {
                   <img
                     src="https://c1.staticflickr.com/9/8387/8638813125_3cac0dc01c_n.jpg"
                     width="174"
+                    height="115"
                     alt="parrot"
                   />
                 </PhotoCard>
@@ -90,7 +111,7 @@ function App() {
                 <Card width={[1/4, 1]} bg="red" borderRadius={3} p={2} style={{height: '50px', textAlign: "center"}}>Text</Card>
               </Flex>
             </Flex>
-              <DesignCanvas bg="gray" flex={2} p={2}/>
+            <DesignCanvas/>
             <MediaQuery query={`(min-width: ${theme.breakpoints[0]})`}>
               <Box bg="purple" p={2} width={40}>
                 Toolbar
@@ -132,6 +153,7 @@ const PhotoCard = styled(Card).attrs({
   mb: [0, 1],
   width: ['auto', 1]
 })`
+  height: 115px;
   ${props => `
     img {
       width: 100%;
